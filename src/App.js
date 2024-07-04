a91fc7bb7f63d120b03fbba73ee5a474246cd52a
xtom <timmy000728@gmail.com>
2024-07-04 11:44:45 +0800

    new branch profile added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home/home';
 import About from './components/About/about';
 import Skill from './components/Skill/skill';
 import Education from './components/Eduction/education';
+import Qulification from './components/Qualification/Qulification';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <About/>
       <Skill/>
       <Education/>
+      <Qulification/>
     </main>
     </>
   );
